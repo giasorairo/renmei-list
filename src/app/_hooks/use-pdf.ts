1ec05e91ab4238f2924501bf127cbc607cef1ee6
yanagi <ashday7188@gmail.com>
2023-06-18 19:02:16 +0900

    feat: raect-pdf-renderer で pdf 生成処理実装

--- src/app/_hooks/use-pdf.ts
+++ src/app/_hooks/use-pdf.ts
@@ -1,38 +1,13 @@
-import { download } from "@/utilities/download";
-import { useCallback, useState } from "react";
-
-const preview = (blob: Blob) => {
-  const url = URL.createObjectURL(blob);
-  window.open(url);
-};
+import { PdfController } from "@/utilities/pdf-controller";
+import { ReactElement, useCallback } from "react";
 
 export const usePdf = (names: string[], company: string, department: string) => {
-  const [loading, setLoading] = useState(false);
+  const printPdf = useCallback((document: ReactElement<Document>) => {
+    PdfController.print(document);
+  }, []);
 
-  const downloadPdf = useCallback(() => {
-    fetch('/api/pdf', {
-      method: 'POST',
-      body: JSON.stringify({ names, company, department }),
-    })
-      .then((res) => res.blob())
-      .then(async (blob) => {
-        download(blob, 'download.pdf');
-      });
-  }, [company, department, names]);
-
-  const printPdf = useCallback(async () => {
-    setLoading(true);
-    await fetch('/api/pdf', {
-      method: 'POST',
-      body: JSON.stringify({ names, company, department }),
-    })
-      .then((res) => res.blob())
-      .then(async (blob) => {
-        preview(blob);
-      })
-      .catch(() => {});
-    setLoading(false);
-  }, [company, department, names]);
-
-  return { downloadPdf, printPdf, loading };
+  const downloadPdf = useCallback((document: ReactElement<Document>) => {
+    PdfController.download(document, 'download.pdf');
+  }, []);
+  return { downloadPdf, printPdf };
 };
