f0758a143f27c85310965607f2abc68f81d65588
yanagi <ashday7188@gmail.com>
2023-06-22 01:51:26 +0900

    feat: pwa 対応

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const withPWA = require("next-pwa")({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NEXT_PUBLIC_NODE_ENV === "development",
+});
 
-module.exports = nextConfig
+module.exports = withPWA({});
