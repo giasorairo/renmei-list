6d2052609c24c32d5ecefd543f9ee0db699ccd51
yanagi <ashday7188@gmail.com>
2023-06-24 18:36:54 +0900

    fix: localStorageController

--- src/utilities/local-storage-controller.ts
+++ src/utilities/local-storage-controller.ts
@@ -2,11 +2,11 @@ export class LocalStorageController {
 
   static get<T>(key: string): T | null | string {
     const value = localStorage.getItem(key);
+    if (!value) {
+      return null;
+    }
     try {
-      const json = JSON.parse(value || '');
-      if (json === 'undefined' || json === 'null') {
-        return null;
-      }
+      const json = JSON.parse(value);
       return json;
     } catch (e) {
       return value as string;
@@ -14,6 +14,17 @@ export class LocalStorageController {
   }
 
   static set<T>(key: string, value: T) {
+    if (value === null
+      || value === undefined
+      || (typeof value === 'number' && isNaN(value))
+    ) {
+      localStorage.removeItem(key);
+      return;
+    }
+    if (typeof value === 'string') {
+      localStorage.setItem(key, value);
+      return;
+    }
     const stringValue = JSON.stringify(value);
     localStorage.setItem(key, stringValue);
   }
