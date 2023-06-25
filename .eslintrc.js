122ee792795f53a4091fc08c3433ad30b08b7c84
gia <giasorairo2015@gmail.com>
2023-06-25 12:45:45 +0900

    feat: add lint, prettier

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+/** @type {import('@typescript-eslint/experimental-utils').TSESLint.Linter.Config} */
+
+const config = {
+  extends: ['airbnb', 'next/core-web-vitals', 'prettier'],
+};
+
+module.exports = config;
